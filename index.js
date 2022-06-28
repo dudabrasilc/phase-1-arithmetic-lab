4b123e3bc2fceceeba7d321855b4c03b1405f9fc
Maria Coitinho <brasildu@gmail.com>
2022-06-28 18:36:43 -0500

    commit tree 1ff9d259465ac7e6dadd92b63745dbf8e3d2c451

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Equation whose multiplied results will equal 62
+let num1 = 1;
+let num2 = 62;
+let multiply = num1*num2;
+
+// Generates a random integer greater than 0
+let random = Math.round(Math.random() * 10);
+
+// Is an equation that calculates a remainder that it's equal to 4
+let num3 = 13;
+let num4 = 9;
+let mod = num3%num4;
+
+// Will return 20 as the highest number in the set
+let max = Math.max(10, 15, 3, 20);
